177aebf38e78ed5bdb90bd1e13d04fc080cae85e
Reynard Hansel <92787298+ReynardHansel@users.noreply.github.com>
2025-02-05 15:56:57 +0700

    Downgraded to Tailwind v3.4.17 to work with ShadCN
    - Succesfully installed Button component to try

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+// import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
